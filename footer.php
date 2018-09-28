004b7f195dddf54a4c1c9d6e3c182dc318a4653d
Geoffroy <geoffroy.c@codeur.online>
2018-09-28 10:56:25 +0200

    Ajout footer.php sur chaque pages

--- footer.php
+++ footer.php
@@ -38,10 +38,6 @@
 						</footer>
 		</div>
 </div>
-		<footer>
-			<p class="made_with_love">Made with <span class="coeur">❤</span> by Jeanne, Gaëtan, Valérie & Geoffroy</p>
-			<p class="bottom_link">
-			Powered by <a href="http://www.accesscodeschool.fr/" target="_blank"><img class="acs_logo" src="media/acs.jpg" width="400" height="400" alt="Access code school logo"></a>
-		</footer>
+<?php include("footer.inc.php"); ?>
 </body>
 </html>

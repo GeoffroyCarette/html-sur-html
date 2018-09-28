cef1eae21bcdc988604c16419e377c0cb324ce7d
Geoffroy <geoffroy.c@codeur.online>
2018-09-28 11:06:22 +0200

    Modification lien en php

--- menu.php
+++ menu.php
@@ -1,20 +1,20 @@
 <div class="sidenav">
 						<nav>
 							<h4>Balise de type bloc :</h4>
-							<a href="div.html">&lt;div&gt;</a>
-							<a href="footer.html">&lt;footer&gt;</a>
-							<a href="header.html">&lt;header&gt;</a>
-							<a href="h1ah6.html">&lt;h1&gt; à &lt;h6&gt;</a>
-							<a href="p.html">&lt;p&gt;</a>
-							<a href="table.html">&lt;table&gt;</a>
-							<a href="ul.html">&lt;ul&gt;</a>
+							<a href="div.php">&lt;div&gt;</a>
+							<a href="footer.php">&lt;footer&gt;</a>
+							<a href="header.php">&lt;header&gt;</a>
+							<a href="h1ah6.php">&lt;h1&gt; à &lt;h6&gt;</a>
+							<a href="p.php">&lt;p&gt;</a>
+							<a href="table.php">&lt;table&gt;</a>
+							<a href="ul.php">&lt;ul&gt;</a>
 						<h4>Balise de type inline :</h4>	
-							<a href="img.html">&lt;img&gt;</a>
-							<a href="strong.html">&lt;strong&gt;</a>
-							<a href="textarea.html">&lt;textarea&gt;</a>
+							<a href="img.php">&lt;img&gt;</a>
+							<a href="strong.php">&lt;strong&gt;</a>
+							<a href="textarea.php">&lt;textarea&gt;</a>
 						<h4>Balises de type indéfini :</h4>
-							<a href="head.html">&lt;head&gt;</a>
-							<a href="title.html">&lt;title&gt;</a>
-							<a href="html.html">&lt;html&gt;</a>
+							<a href="head.php">&lt;head&gt;</a>
+							<a href="title.php">&lt;title&gt;</a>
+							<a href="html.php">&lt;html&gt;</a>
 						</nav>
 					</div>            

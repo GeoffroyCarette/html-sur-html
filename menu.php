4805e9a8ae2e0448ed3c9fbf18b1e27db6e9ad40
JeanneT <jeanne.t@codeur.online>
2018-09-28 10:36:53 +0200

    [php]
    Menu sur le coté en php include

---
+++ menu.php
@@ -0,0 +1,20 @@
+<div class="sidenav">
+						<nav>
+							<h4>Balise de type bloc :</h4>
+							<a href="div.html">&lt;div&gt;</a>
+							<a href="footer.html">&lt;footer&gt;</a>
+							<a href="header.html">&lt;header&gt;</a>
+							<a href="h1ah6.html">&lt;h1&gt; à &lt;h6&gt;</a>
+							<a href="p.html">&lt;p&gt;</a>
+							<a href="table.html">&lt;table&gt;</a>
+							<a href="ul.html">&lt;ul&gt;</a>
+						<h4>Balise de type inline :</h4>	
+							<a href="img.html">&lt;img&gt;</a>
+							<a href="strong.html">&lt;strong&gt;</a>
+							<a href="textarea.html">&lt;textarea&gt;</a>
+						<h4>Balises de type indéfini :</h4>
+							<a href="head.html">&lt;head&gt;</a>
+							<a href="title.html">&lt;title&gt;</a>
+							<a href="html.html">&lt;html&gt;</a>
+						</nav>
+					</div>            
